df1e9c67d98798c5253fc34d08dac51b33711d2b
creativeothman <crativeothman@gmail.com>
2017-02-20 20:41:49 +0100

    user profile card added

--- assets/js/eneg.js
+++ assets/js/eneg.js
@@ -1,4 +1,9 @@
 
+$(document).ready(function(){
 
+//$('.panel').removeClass();
+
+
+});
 
 
---
+++ private/user/User_config.php
@@ -0,0 +1,40 @@
+
+
+<!doctype html>
+<html>
+<head>
+    <title>Welcome to Expanded Wings Login Page:</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/eneg.css">
+    
+<style>
+    
+    
+</style>   
+<script>
+
+</script> 
+</head>
+    <body>
+    <div class="wrapper">
+    <header>
+        <div class="logo">
+        
+        </div>
+            <nav>
+            <ul>
+             </ul>
+        </nav>
+        
+    </header>
+    <aside class="left_panel"></aside>
+    <aside class="right_panel"></aside>
+    <div class="content">
+   
+                
+        
+        
+        </div>
+        <footer> &copy; 2017 </footer>
+        </div>
+    </body>
+</html>
